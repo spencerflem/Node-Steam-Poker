2d9179d5077572085ca2c4d286cb02cbbea31c45
Spencer <spencerflem@gmail.com>
2014-10-18 23:57:54 -0400

    Added Express

--- Steamier.js
+++ Steamier.js
@@ -1,6 +1,18 @@
 ﻿var fs = require('fs')
 var Steam = require('steam')
 var _ = require('lodash')
+var express = require('express')
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+app.get('/', function(request, response) {
+response.send('Hello World!')
+})
+app.listen(app.get('port'), function() {
+console.log("Node app is running at localhost:" + app.get('port'))
+})
 
 var startedRooms = []
 
